f329dd6ea4c99294ed6f02157af189884edc0bef
Apachedrag427 <93100547+Apachedrag427@users.noreply.github.com>
2022-04-22 17:18:18 -0500

    Create chatCodes.lua

---
+++ chatCodes.lua
@@ -0,0 +1,16 @@
+local code = string.char(167)
+local chat = peripheral.wrap("left")
+chat.capture("")
+pull = os.pullEvent
+os.pullEvent = os.pullEventRaw
+local function grab()
+	local event, msg, _, plr, uuid = os.pullEvent()
+	if event == "terminate" then return "terminated" elseif event ~= "chat_capture" then return end
+	msg = msg:gsub("&", code)
+	chat.say(msg)
+end
+while true do
+	if grab() == "terminated" then break end
+end
+os.pullEvent = pull
+chat.clearCaptures()
