cfb6e52de0d68c7ed7d29d17fd6370762f9f1f9c
Apachedrag427 <connermccoy37@gmail.com>
2022-04-23 16:57:31 -0500

    Update mdTest.lua

--- mdTest.lua
+++ mdTest.lua
@@ -1,6 +1,6 @@
 local handle = http.get("https://raw.githubusercontent.com/Apachedrag427/CC/main/mdTest.lua")
 if handle then
-    local current = fs.open(shell.getRunningProgram(), "r")
+    local current = fs.open(shell.getRunningProgram(), "w")
     if current then
 	    local current = fs.open(shell.getRunningProgram(), "r")
 	    local contents = current.readAll()
