b96b6dd9a5874ed47da7f5502aa17b2370db9e62
Apachedrag427 <93100547+Apachedrag427@users.noreply.github.com>
2022-05-29 16:45:24 -0500

    Update say.lua

--- say.lua
+++ say.lua
@@ -1,47 +1,4 @@
-local args = {...}
-local returnWhenUpdated = false
-if args[1] == "update" then
-    returnWhenUpdated = true
-end
-if not http then
-	error("Please enable http in config.", 0)
-end
-local function get(url)
-	local response = http.get(url)
-	if not response then
-		error("Could not get " .. url, 0)
-	end
-	local data = response.readAll()
-	response.close()
-	return data
-end
-local new = get("https://raw.githubusercontent.com/Apachedrag427/CC/main/say.lua")
-local old = fs.open("/say.lua", "r").readAll()
-if new ~= old then
-	print("Update detected, press 'y' to update, press 'n' to refuse")
-	local _, key = os.pullEvent("key")
-	if key == keys.y then
-		local file = fs.open("/say.lua", "w")
-    	if file then
-        	file.write(new)
-        	file.close()
-        	print("Updated")
-        	sleep(1)
-        	os.reboot()
-    	end
-	else
-		print("Not updating")
-	end
-end
-if returnWhenUpdated then return end
-local modules = peripheral.find("manipulator")
-if not modules then
-    error("Please put a manipulator with a creative chat recorder around this computer.", 0)
-end
-if not modules.hasModule("plethora:chat_creative") then
-    error("Please put a creative chat recorder around this computer.", 0)
-end
-local chat = modules
+local chat = peripheral.find("manipulator")
 local code = string.char(167)
 local prefix
 if not fs.exists(".prefix") then
